dbf63786a6cc319edeb405c0fec628bd9b8de959
ARYAN-Z1 <110971320+ARYAN-Z1@users.noreply.github.com>
2024-05-09 13:19:32 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,5 +1,17 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_message(s: str, p: str) -> bool:
 
-# write your code here
-  
+    if len(s) != len(p):
         return False
+
+    for i in range(len(s)):
+        message_char = s[i]
+        key_char = p[i]
+        
+        if message_char == key_char or (key_char == '?' and message_char.isalpha()):
+            continue
+        elif key_char == '*':
+            continue
+        else:
+            return False 
+
+    return True
